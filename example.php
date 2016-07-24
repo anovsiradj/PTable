43cb8149b21450dd54f4cc8a164b655ec5543758
anovsiradj <anov.siradj22@gmail.com>
2016-07-24 16:34:08 +0700

    ganti inc example dengan autoload
    dan contoh custom tag

--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?php
-require 'src/PTable.php';
+require('vendor/autoload.php');
 
 $data = [
 	["Data A","Data B","Data C"],
@@ -11,7 +11,7 @@ $data = [
 	["Data I","Data J","Data K"]
 ];
 
-$table = new avz\PTable\PTable;
+$table = new avz\PTable\PTable('<table class="light">');
 
 $thead = $table->thead();
 	$tr = $thead->tr();
@@ -23,7 +23,7 @@ $index = 0;
 $tbody = $table->tbody();
 	foreach ($data as $n) {
 		$tr = $tbody->tr();
-			$custom_td_tag = ($index%2 === 0) ? '<td class="bggreen">' : '<td>';
+			$custom_td_tag = ($index%2 === 0) ? '<td class="bggreen">' : '<td style="color:#222;">';
 			$tr->td($n,$custom_td_tag);
 		$tr->close();
 		$index++;
@@ -32,15 +32,12 @@ $tbody->close();
 $mytable = $table->render();
 
 echo <<<HTML
-<pre>
 <style>
-	table {font-size: 32px;max-width:100%;}
-	table td,table th {padding:5px 10px;}
-	table td {border:1px solid red;}
-	table th {border:1px solid blue;}
-	.bgblue {background-color: #0099ff;}
-	.bggreen {background-color: #669900;}
-</style>
+	body {background-color: #222;color: #fff;font-family: monospace;}
+	table {font-size: 32px;max-width:100%;}table.light {background-color: #fff;}
+	table td,table th {padding:5px 10px;}table td {border:1px solid red;}table th {border:1px solid blue;}
+	.bgblue {background-color: #0099ff;}.bggreen {background-color: #669900;}
+</style><pre>
 HTML;
 
 echo $mytable;
